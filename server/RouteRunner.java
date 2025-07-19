e1e5851691b3a0ae14a084184d33effd5e1b66a8
JohnTheCreator <jo214841@gmail.com>
2025-07-19 17:15:19 +0530

    del

--- server/RouteRunner.java
+++
@@ -1,8 +0,0 @@
-package server;
-
-import request.HttpRequest;
-import response.HttpResponse;
-
-public interface RouteRunner {
-    HttpResponse.Builder run(HttpRequest var1, HttpResponse.Builder var2);
-}
